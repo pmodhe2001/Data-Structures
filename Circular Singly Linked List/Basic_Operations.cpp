480a5df6ad062b35a6deb7d9d3da76f75567b724
Prathmesh Modhe <49117568+pmodhe2001@users.noreply.github.com>
2022-01-10 10:35:30 +0530

    Update Basic_Operations.cpp

--- Circular Singly Linked List/Basic_Operations.cpp
+++ Circular Singly Linked List/Basic_Operations.cpp
@@ -1,42 +1,69 @@
-
-
 #include<iostream>
 using namespace std;
 
 class CNode{
     public:
        int data;
-       Node *next;
+       CNode *next;
        CNode(int data){
            this->data=data;
            next=NULL;
        }
+};
+
+CNode *addtoempty(CNode *last,int data){
+    if(last!=NULL){
+        return last;
+    }
+    CNode *newNode = new CNode(data);
+    last=newNode;
+    last->next=newNode;
+    return last;
 }
 
-void print(CNode *head){
-    CNode *tmp=head;
-    while(true){
-        cout<<tmp->data<<" ";
-        tmp=tmp->next;
-        if(tmp==head){
-            break;
-        }
-    }
-    cout<<endl;
-}
-
-CNode *takeInput_atfront(CNode *head){
-    CNode *tail=NULL;
-    CNode *newNode= new CNode(data);
-    if(head==NULL){
-        head=newNode;
-        newNode->next=newNode;
-    }
-    else{
-        CNode *tmp=head;
+CNode *addtofront(CNode *last,int data){
+    if(last==NULL){
+        return addtoempty(last,data);
         
     }
+    CNode *newNode = new CNode(data);
+    newNode->next=last->next;
+    last->next=newNode;
+    return last;
 }
+
+CNode *addEnd(CNode *last,int data){
+    if(last==NULL){
+        return addtoempty(last,data);
+    }
+    CNode *newNode = new CNode(data);
+    newNode->next=last->next;
+    last->next=newNode;
+    last=newNode;
+    return newNode;
+}
+
+void print(CNode *tail){
+    if(tail==NULL){
+        return;
+    }
+    CNode *tmp = tail->next;
+
+    do {
+       cout << tmp->data << " ";
+       tmp = tmp->next;
+    } 
+    while (tmp != tail->next);
+}
+
+
 int main(){
-    
+    CNode *last=NULL;
+   last = addtoempty(last, 6);
+  last = addEnd(last, 8);
+  last = addtofront(last, 2);
+  last = addEnd(last, 10);
+  last = addEnd(last, 12);
+    print(last);
 }
+    
