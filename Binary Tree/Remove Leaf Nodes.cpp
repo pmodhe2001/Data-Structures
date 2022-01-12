b02643a505cbd08e9530073210afd8fe71c0126e
Prathmesh Modhe <49117568+pmodhe2001@users.noreply.github.com>
2022-01-12 18:32:08 +0530

    Update Remove Leaf Nodes.cpp

--- Binary Tree/Remove Leaf Nodes.cpp
+++ Binary Tree/Remove Leaf Nodes.cpp
@@ -1,3 +1,9 @@
+/*
+Given a binary tree, remove all leaf nodes from it. Leaf nodes are those nodes, which don't have any children.
+Note:
+1. Root will also be a leaf node if it doesn't have left and right child. 
+2. You don't need to print the tree, just remove all leaf nodes and return the updated root.
+*/
 BinaryTreeNode<int>* RemoveLeafNodes(BinaryTreeNode<int> *root){
     if(root==NULL){
         return NULL;
