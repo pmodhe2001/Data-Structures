70f7f461726212e8cf73bdcecbb2adc6880fe266
Prathmesh Modhe <49117568+pmodhe2001@users.noreply.github.com>
2022-01-12 18:49:39 +0530

    Update Pair Sum in BST.cpp

--- Binary Search Tree/Pair Sum in BST.cpp
+++ Binary Search Tree/Pair Sum in BST.cpp
@@ -6,36 +6,44 @@ Note:
 */
 
 // Approach 1 : Sorting Based
-#include<algorithm>
-#include<vector>
-
-void convertToArray(BinaryTreeNode<int> *root,vector<int> &output){
-    if(root==NULL){
+#include<bits/stdc++.h>
+void helper(BinaryTreeNode<int> *root, vector<int> &arr){
+	if(root == NULL){
         return;
     }
-    output.push_back(root->data);
-    convertToArray(root->left,output);
-    convertToArray(root->right,output);
     
+    
+    
+    helper(root -> left, arr);
+    arr.push_back(root -> data);
+    helper(root -> right, arr);
 }
 
-void pairSum(BinaryTreeNode<int> *root,int sum){
+void printNodesSumToS(BinaryTreeNode<int> *root, int s) {
+    
     vector<int> output;
-    convertToArray(root,output);
-    sort(output.begin(),output.end());
-    int i=0,j=output.size()-1;
-    while(i<j){
-        if(output[i]+output[j]==sum){
-            cout<<output[i]<<" "<<output[j]<<endl;
-            i++;j--;
+    
+    helper(root, output);
+    
+    //sort(output.begin(), output.end());
+    
+    int i = 0; int j = output.size() - 1;
+    while(i < j){
+        
+        if(output[i] + output[j] > s){
+            j--;
         }
-        else if(output[j]+output[i]<sum){
+        else if(output[i] + output[j] < s){
             i++;
         }
         else{
-            j++;
+            cout << output[i] << " " << output[j] << endl;
+            i++;
+            j--;
         }
     }
+
 }
+
 
 // Approach 2 : Hashing Based
