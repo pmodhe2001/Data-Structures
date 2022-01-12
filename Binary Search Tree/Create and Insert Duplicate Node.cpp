329ce8772e80dadaba1418937630829877382c54
Prathmesh Modhe <49117568+pmodhe2001@users.noreply.github.com>
2022-01-12 18:44:35 +0530

    Update Create and Insert Duplicate Node.cpp

--- Binary Search Tree/Create and Insert Duplicate Node.cpp
+++ Binary Search Tree/Create and Insert Duplicate Node.cpp
@@ -1,4 +1,7 @@
-void InsertDuplicateNode(BinaryTreeNode<int> *root){
+/*
+For a given a Binary Tree of type integer, duplicate every node of the tree and attach it to the left of itself.
+The root will remain the same. So you just need to insert nodes in the given Binary Tree.
+*/void InsertDuplicateNode(BinaryTreeNode<int> *root){
     if(root==NULL){
         return;
     }
