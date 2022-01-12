451ba0fcf7aa2d846a137f912dc11441344d94ee
Prathmesh Modhe <49117568+pmodhe2001@users.noreply.github.com>
2022-01-12 18:31:11 +0530

    Update Sum of Nodes.cpp

--- Binary Tree/Sum of Nodes.cpp
+++ Binary Tree/Sum of Nodes.cpp
@@ -1,4 +1,14 @@
+/*
+For a given Binary Tree of integers, find and return the sum of all the nodes data.
+Example:
+                        10
+                      /      \
+                    20      30
+                   /    \
+                 40    50
 
+When we sum up all the nodes data together, [10, 20, 30, 40 50] we get 150. Hence, the output will be 150.
+*/
 int getSum(BinaryTreeNode<int> *root){
     if(root==NULL){
         return 0;
