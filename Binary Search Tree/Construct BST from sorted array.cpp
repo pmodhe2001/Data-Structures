7058e90eee4f968a815b20bbedcc08168bf7f726
Prathmesh Modhe <49117568+pmodhe2001@users.noreply.github.com>
2022-01-12 18:46:52 +0530

    Update Construct BST from sorted array.cpp

--- Binary Search Tree/Construct BST from sorted array.cpp
+++ Binary Search Tree/Construct BST from sorted array.cpp
@@ -1,3 +1,9 @@
+/*
+Code: Construct BST from a Sorted Array
+Send Feedback
+Given a sorted integer array A of size n, which contains all unique elements. You need to construct a balanced BST from this input array. Return the root of constructed BST.
+Note: If array size is even, take first mid as root.
+*/
 BinaryTreeNode<int> *tree(int *input, int start, int end){
     if(start > end){    //base case
         return NULL;
