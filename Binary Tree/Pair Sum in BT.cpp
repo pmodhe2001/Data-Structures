c0e7d6dbd557c84a038fc9d8cbb97fc7ed1d3eb5
Prathmesh Modhe <49117568+pmodhe2001@users.noreply.github.com>
2022-01-13 15:53:55 +0530

    Update Pair Sum in BT.cpp

--- Binary Tree/Pair Sum in BT.cpp
+++ Binary Tree/Pair Sum in BT.cpp
@@ -1,1 +1,35 @@
+/*
+Pair Sum Binary Tree
 
+Given a binary tree and an integer S, print all the pair of nodes whose sum equals S.
+Note:
+1. Assume the given binary tree contains all unique elements.
+2. In a pair, print the smaller element first. Order of different pairs doesn't matter.
+*/
+#include <algorithm>
+#include <vector>
+void convertToArray(BinaryTreeNode<int> *root, vector<int> &output) { 
+    if (root == NULL) { 
+        return; 
+    }
+    convertToArray(root->left, output); 
+    output.push_back(root->data); 
+    convertToArray(root->right, output); 
+}
+void pairSum(BinaryTreeNode<int> *root, int sum) { 
+    vector<int> output; convertToArray(root, output); 
+    sort(output.begin(), output.end()); 
+    int i = 0, j = output.size() - 1; 
+    while (i < j) { 
+        if (output[i] + output[j] == sum) { 
+            cout << output[i] << " " << output[j] << "\n";
+            ++i; --j; 
+        }
+        else if (output[i] + output[j] > sum) {
+            --j; 
+        }
+        else {
+            ++i; 
+        } 
+    } 
+}
