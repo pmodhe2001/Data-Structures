18ae2ebc936f16fbacf40578eb8cbd9b35c642d3
Prathmesh Modhe <49117568+pmodhe2001@users.noreply.github.com>
2022-01-14 20:52:07 +0530

    Create Power.cpp

---
+++ Recursion/Power.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int power(int x,int n){
+    if(n==0){
+        return 1 ;
+    }
+    int smallans=power(x,n-1);
+    return x*smallans;
+}
+int main(){
+    int x, n;
+    cin >> x >> n;
+  
+    cout << power(x, n) << endl;
+}
