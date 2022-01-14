6566c56c3f98653e06394a981531a992c9c7256d
Prathmesh Modhe <49117568+pmodhe2001@users.noreply.github.com>
2022-01-14 20:54:56 +0530

    Create Print Numbers.cpp

---
+++ Recursion/Print Numbers.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void print(int n){
+    if(n==0){
+        return;
+    }
+    print(n-1);
+    cout<<n<<" ";
+}
+
+int main(){
+    int n;
+    cin >> n;
+  
+    print(n);
+}
+
+
