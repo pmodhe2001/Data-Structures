6f97ce493c5b8415b30246b656cdea5970be6ebe
Prathmesh Modhe <49117568+pmodhe2001@users.noreply.github.com>
2022-01-16 00:14:57 +0530

    Create Pairs with difference K.cpp

---
+++ Hashmaps/Pairs with difference K.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include<unordered_map>
+
+using namespace std;
+
+int getPairsWithDifferenceK(int *arr, int n, int k) {
+	// Write your code here
+    int ans = 0;
+	unordered_map<int, int>mp;
+    
+    for(int i = 0;i < n; i++){
+		int c = arr[i] + k;
+        ans += mp[c];
+        if(k != 0){
+            int c = arr[i] - k;
+            ans += m[c];
+        }
+        m[arr[i]]++;
+	}
+	return ans;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int *input = new int[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> input[i];
+    }
+
+    int k;
+    cin >> k;
+
+    cout << getPairsWithDifferenceK(input, n, k);
+
+    delete[] input;
+}
