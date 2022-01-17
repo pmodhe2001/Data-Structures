e463d01d6c04ffb6c91355aaecec4473521574d3
Prathmesh Modhe <49117568+pmodhe2001@users.noreply.github.com>
2022-01-17 20:28:03 +0530

    Create Leaf at same level.cpp

---
+++ Binary Tree/Leaf at same level.cpp
@@ -0,0 +1,18 @@
+int height(Node *root,bool &res){
+        if(root==NULL)return 0;
+        int left=height(root->left,res);
+        int right=height(root->right,res);
+        if(left!=NULL && right!=NULL && left!=right){
+            res=false;
+        }
+        return 1+max(left,right);
+    }
+    bool check(Node *root)
+    {
+        if(root==NULL){
+            return root;
+        }
+        bool check_level=true;
+        height(root,check_level);
+        return check_level;
+    }
