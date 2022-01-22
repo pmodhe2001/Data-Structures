7a308115dde89ba32720cea667aa7ecf053eb295
Prathmesh Modhe <49117568+pmodhe2001@users.noreply.github.com>
2022-01-22 16:33:02 +0530

    Create LCA of BT.cpp

---
+++ Binary Tree/LCA of BT.cpp
@@ -0,0 +1,20 @@
+ TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+        if(!root)return NULL;
+        
+        if(root->val==p->val || root->val==q->val){
+            return root;   
+        }
+        
+        TreeNode* left=lowestCommonAncestor(root->left,p,q);
+        TreeNode* right=lowestCommonAncestor(root->right,p,q);
+        
+        if(!left){
+            return right;
+        }
+        
+        if(!right){
+            return left;
+        }
+        return root;
+    }
