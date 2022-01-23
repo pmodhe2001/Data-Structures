bd91080b2714b758facf6fa8f131889b7dc3bdca
Prathmesh Modhe <49117568+pmodhe2001@users.noreply.github.com>
2022-01-23 15:49:56 +0530

    Create Flatten Binary tree to singly linked list.cpp

---
+++ Binary Tree/Flatten Binary tree to singly linked list.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    TreeNode *prev=NULL;
+    void flatten(TreeNode* root) {
+        if(!root)return;
+        
+        flatten(root->right);
+        flatten(root->left);
+        root->right=prev;
+        root->left=NULL;
+        prev=root;
+        
+    }
+};
