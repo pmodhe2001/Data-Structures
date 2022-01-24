1fd818a282e0487e4bd7beb105c91b0da1797d40
Prathmesh Modhe <49117568+pmodhe2001@users.noreply.github.com>
2022-01-24 21:39:35 +0530

    Create Remove duplicates from an unsorted linked list.cpp

---
+++ Singly Linked List/Remove duplicates from an unsorted linked list.cpp
@@ -0,0 +1,52 @@
+//https://practice.geeksforgeeks.org/problems/remove-duplicates-from-an-unsorted-linked-list/1#
+
+//Approach 1 Using Hashmaps
+Node * removeDuplicates( Node *head) 
+    {
+       if(!head)return head;
+       
+       Node *tmp=head;
+       unordered_map<int,int> freq;
+       
+       freq[tmp->data]=1;
+       
+       while(tmp!=NULL && tmp->next!=NULL){
+           if(freq.count(tmp->next->data)==1){
+               tmp->next=tmp->next->next;
+           }
+           else{
+               tmp=tmp->next;
+               freq[tmp->data]++;
+           }
+       }
+       
+       return head;
+    }
+
+/*Approach 2 Using Unordered Set :
+Giving wrong answer of 1 test case : 
+1
+23
+*/
+Node * removeDuplicates( Node *head) 
+    {
+       if(head==NULL || head->next==NULL){
+           return NULL;
+       }
+       
+       unordered_set<int> unique;
+       Node *temp=head;
+       //unique.insert(head->data);
+       while(temp->next!=NULL){
+           unique.insert(temp->data);
+           if(unique.find(temp->next->data)!=unique.end()){
+               temp->next=temp->next->next;
+           }
+           else{
+               temp=temp->next;
+           }
+       }
+       
+       return head;
+    }
+// Approach 3 Using 2 loops
