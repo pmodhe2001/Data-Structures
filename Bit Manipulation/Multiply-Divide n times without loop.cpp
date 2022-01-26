f74465da6709835a61a010cc3c4eb3ac9d43a33a
Prathmesh Modhe <49117568+pmodhe2001@users.noreply.github.com>
2022-01-26 13:43:20 +0530

    Create Multiply-Divide n times without loop.cpp

---
+++ Bit Manipulation/Multiply-Divide n times without loop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int m,n;
+    cin>>m>>n;
+    
+    cout<<"Multiply"<<" "<<(m<<n)<<endl;
+    
+    cout<<"Divide"<<" "<<(m>>n)<<endl;
+    
+}
