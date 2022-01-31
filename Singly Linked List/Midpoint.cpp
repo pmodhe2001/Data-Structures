5eeeea3e32f18a5879a11bdd3d56b7581cecd2c6
Prathmesh Modhe <49117568+pmodhe2001@users.noreply.github.com>
2022-01-31 15:54:04 +0530

    Update Midpoint.cpp

--- Singly Linked List/Midpoint.cpp
+++ Singly Linked List/Midpoint.cpp
@@ -1,95 +1,23 @@
-#include<iostream>
-using namespace std;
-
-class Node{
-  public:
-    int data;
-    Node *next;
-    Node(int data){
-        this->data=data;
-        this->next=NULL;
+https://practice.geeksforgeeks.org/problems/finding-middle-element-in-a-linked-list/1/#
+class Solution{
+    public:
+    /* Should return data of middle node. If linked list is empty, then  -1*/
+    int getMiddle(Node *head)
+    {
+        if(head==NULL){
+            return -1;
+        }
+        
+        Node *slow=head;Node *fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                break;
+            }
+        }
+        
+        return slow->data;
     }
 };
-
-Node *takeInput(){
-    int data;
-    cin>>data;
-    Node *head=NULL,*tail=NULL;
-    while(data!=-1){
-        Node *newNode=new Node(data);
-        if(head==NULL){
-            head=newNode;
-            tail=newNode;
-        }else{
-            tail->next=newNode;
-            tail=newNode;
-        }
-        cin>>data;
-    }
-    return head;
-}
-
-void print_recursively(Node *head){
-    Node *temp=head;
-    if(temp==NULL){
-        return;
-    }
-    cout<<temp->data<<" ";
-    print_recursively(temp->next);
-}
-
-int length(Node *head){
-    if(head==NULL){
-        return 0;
-    }
-    return length(head->next)+1;
-}
-
-Node *appendLastNtofirst(Node *head,int n){
-  if(n==0 || head==NULL){
-      return head;
-  }
-    
-   Node *fast=head;
-   Node *slow=head;
-   Node *initialhead=head;
-    
-   for(int i=0;i<n;i++){
-       fast=fast->next;
-   }
-    
-   while(fast->next!=NULL){
-      slow=slow->next;
-      fast=fast->next;
-   }
-    
-    Node *temp=slow->next;
-    slow->next=NULL;
-    fast->next=initialhead;
-    head=temp;
-    return head;
-}
-
-Node *midpoint(Node *head){
-    if(head==NULL){
-        return head;
-    }
-    
-    Node *slow=head;Node *fast=head->next;
-    while(head!=NULL && fast!=NULL && fast->next!=NULL){
-        slow=slow->next;
-        fast=fast->next->next;
-    }
-    return slow;
-}
-
-int main(){
-    Node *head=takeInput();
-    Node *mid=midpoint(head);
-    if(mid!=NULL){
-        cout<<mid->data<<" ";
-    }
-    else{
-        cout<<"NULL LIST"<<endl;
-    }
-}
