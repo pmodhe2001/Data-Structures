722f155d94fe30057507f1ad644679e3b4cd9fbc
Prathmesh Modhe <49117568+pmodhe2001@users.noreply.github.com>
2022-01-31 11:32:31 +0530

    Update Basic_Operations.cpp

--- Circular Singly Linked List/Basic_Operations.cpp
+++ Circular Singly Linked List/Basic_Operations.cpp
@@ -1,69 +1,123 @@
 #include<iostream>
 using namespace std;
 
-class CNode{
-    public:
-       int data;
-       CNode *next;
-       CNode(int data){
-           this->data=data;
-           next=NULL;
-       }
+class Node{
+  public:
+     int data;
+     Node *next=NULL;
+     Node(int data){
+         this->data=data;
+         Node *next=NULL;
+     }
 };
 
-CNode *addtoempty(CNode *last,int data){
-    if(last!=NULL){
-        return last;
+Node *takeInput(Node *&tail){
+    int data;
+    cin>>data;
+    while(data!=-1){
+        if(tail==NULL){
+            Node *newNode=new Node(data);
+            newNode->next=newNode;
+            tail=newNode;
+        }
+        else{
+            Node *newNode=new Node(data);
+            newNode->next=tail->next;
+            tail->next=newNode;
+            tail=newNode;
+        }
+        cin>>data;
     }
-    CNode *newNode = new CNode(data);
-    last=newNode;
-    last->next=newNode;
-    return last;
+    
+    return tail;
 }
 
-CNode *addtofront(CNode *last,int data){
-    if(last==NULL){
-        return addtoempty(last,data);
+Node *takeInput_Begg(Node *tail){
+    int data;
+    cin>>data;
+    while(data!=-1){
+        if(tail==NULL){
+           Node *newNode=new Node(data);
+           newNode->next=newNode;
+           tail=newNode;
+        }
+        else{
+           Node *newNode=new Node(data);
+           newNode->next=tail->next;
+           tail->next=newNode;
+        }
+        cin>>data;
+    }
+    
+    return tail;
+}
+void print(Node *&tail){
+    
+    Node *tmp=tail->next;
+    
+    while(tmp!=tail){
+        cout<<tmp->data<<" ";
+        tmp=tmp->next;
         
     }
-    CNode *newNode = new CNode(data);
-    newNode->next=last->next;
-    last->next=newNode;
-    return last;
+    cout<<tail->data<<" ";
+    cout<<endl;
 }
 
-CNode *addEnd(CNode *last,int data){
-    if(last==NULL){
-        return addtoempty(last,data);
-    }
-    CNode *newNode = new CNode(data);
-    newNode->next=last->next;
-    last->next=newNode;
-    last=newNode;
-    return newNode;
-}
-
-void print(CNode *tail){
+Node *delFrombegg(Node *&tail){
     if(tail==NULL){
-        return;
+        return NULL;
     }
-    CNode *tmp = tail->next;
-
-    do {
-       cout << tmp->data << " ";
-       tmp = tmp->next;
-    } 
-    while (tmp != tail->next);
+    Node *tmp_del=tail->next;
+    tail->next=tail->next->next;
+    free(tmp_del);
+    return tail;
 }
 
-
-int main(){
-    CNode *last=NULL;
-   last = addtoempty(last, 6);
-  last = addEnd(last, 8);
-  last = addtofront(last, 2);
-  last = addEnd(last, 10);
-  last = addEnd(last, 12);
-    print(last);
-}
+Node *delFromEnd(Node *&head){
+    if(head==NULL)return NULL;
+    Node *tmp=head;
+    while(tmp->next->next!=head){
+        tmp=tmp->next;
+    }
+    Node* temp_del = tmp->next;
+    tmp->next = head;
+    free(temp_del); 
     
+    
+    return head;
+}
+int main()
+{
+    
+    
+    int choice=-1;
+    cin>>choice;
+    while(choice!=-1){
+        if(choice==1){
+            Node *tail=NULL;
+            tail=takeInput(tail); // Taking Input from end
+            print(tail);
+        }
+        else if(choice==2){
+            Node *tail=NULL;
+            tail=takeInput_Begg(tail);
+            print(tail);
+        }
+        else if(choice==3){
+            Node *tail=NULL;
+            tail=takeInput(tail); // Taking Input from end
+            print(tail);
+            tail=delFrombegg(tail); // Deleting from beggining
+            print(tail);
+        }else if(choice==4){
+            Node *tail=NULL;
+            tail=takeInput(tail); // Taking Input from end
+            print(tail);
+            tail=delFromEnd(tail->next); // Deleting from End
+            print(tail);
+        }
+        cin>>choice;
+    }
+   
+}
